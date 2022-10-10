73dafb7a1929d03e2afeebb966274fc2856eda7d
Francisco Fukuda <francisco.fukuda@outlook.com>
2022-10-10 10:20:01 -0300

    change specific agendas

--- src/routes/agendas/agendaCristinaOlivos.js
+++ src/routes/agendas/agendaCristinaOlivos.js
@@ -1,7 +1,4 @@
 export default function AgendaCristinaOlivos() {
-  return (
-    <main>
-      <iframe style={{width:"1024px", height:"2768px", border:"0"}} title="perro" src="https://outlook.office365.com/owa/calendar/consulta_medica_cristina_olivos@cenumet.cl/bookings/"></iframe>
-    </main>
-  );
+  window.location.assign("http://agendamiento.softwaremedilink.com/agendas/online/885efb1b88ba283f8406e8c1580c6272a617c1a7");
+  return <div></div>;
 }
